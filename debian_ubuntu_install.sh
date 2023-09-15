3abac955557e00bb8ec14323f6f33e2f400c932f
atnnewstv <120329915+atnnewstv@users.noreply.github.com>
2023-09-15 17:08:38 +0600

    Update debian_ubuntu_install.sh

--- debian_ubuntu_install.sh
+++ debian_ubuntu_install.sh
@@ -104,10 +104,12 @@ apt-get -y install apt-transport-https gnupg mosquitto-clients
 apt -y install git
 
 if curl -s cip.cc|grep "中国";then
-#git clone https://gitee.com/link4all_admin/vps.git
- git clone https://jihulab.com/link4all/vps.git 
+##git clone https://gitee.com/link4all_admin/vps.git
+#git clone https://jihulab.com/link4all/vps.git 
+ git clone https://github.com/atnnewstv/minetv-vps.git
 else
-git clone https://github.com/hewenhao2008/vps.git
+#git clone https://github.com/hewenhao2008/vps.git
+ git clone https://github.com/atnnewstv/minetv-vps.git
 fi
 
 cd vps
