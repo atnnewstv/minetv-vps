b694c3e1ba5e0ce971031c40c7d9a4128b132da6
atnnewstv <120329915+atnnewstv@users.noreply.github.com>
2023-09-15 18:46:02 +0600

    Update debian_ubuntu_install.sh

--- debian_ubuntu_install.sh
+++ debian_ubuntu_install.sh
@@ -106,10 +106,10 @@ apt -y install git
 if curl -s cip.cc|grep "中国";then
 ##git clone https://gitee.com/link4all_admin/vps.git
 #git clone https://jihulab.com/link4all/vps.git 
- git clone -c http.sslverify=false https://github.com/atnnewstv/minetv-vps.git
+ git clone https://github.com/atnnewstv/minetv-vps.git
 else
 #git clone https://github.com/hewenhao2008/vps.git
- git clone -c http.sslverify=false https://github.com/atnnewstv/minetv-vps.git
+ git clone https://github.com/atnnewstv/minetv-vps.git
 fi
 
 cd vps
